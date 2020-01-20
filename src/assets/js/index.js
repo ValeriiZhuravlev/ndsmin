d564409da682bd1011a8f7133e39feeb654dede4
Valery Juravlev <valera030219972010@gmail.com>
2020-01-20 20:17:46 +0300

    first commit

---
+++ src/assets/js/index.js
@@ -0,0 +1,7 @@
+// Main js file
+
+
+$(document).ready(() => {
+
+});
+
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// JS
+import 'jquery'
+import './assets/js/index.js'
+
+// SCSS
+import './main.scss'
+
+ // CSS
+
+//  import '../node_modules/wowjs/node_modules/animate.css/source/_base.css'
+//  import '../node_modules/wowjs/node_modules/animate.css/source/fading_entrances/fadeInUp.css'
