9d6416434e7e6c30cff58dab0b46387175aa3cc2
Valery Juravlev <valera030219972010@gmail.com>
2020-05-15 01:38:10 +0300

    change send email

--- src/assets/js/scripts/sendform.js
+++ src/assets/js/scripts/sendform.js
@@ -1,59 +1,44 @@
 const sendform = function() {
 	"use strict";
-	$('.contacts__button').click(function(e) {
-		let regEx = new RegExp(/\+\d\(\d\d\d\)\d\d\d\-\d\d\-\d\d/);
-		if(!(regEx.test($('.contacts__input_phone').val()))) {
-			$('.contacts__input_phone').css('border-bottom', '4px solid #ff7730');
-			e.preventDefault();
-		} else {
-			$('.contacts__input_phone').css('border-bottom', 'none');
 
-		}
-	});
-
-	$('.modal__button').click(function(e) {
-		let regEx = new RegExp(/\+\d\(\d\d\d\)\d\d\d\-\d\d\-\d\d/);
-		if(!(regEx.test($('.modal__input_phone').val()))) {
-			$('.modal__input_phone').css('border-bottom', '4px solid #ff7730');
-			e.preventDefault();
-		} else {
-			$('.modal__input_phone').css('border-bottom', 'none');
-		}
-	});
-
-
-
-	$(".contacts__form").submit(function() { 
-		let th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: th.serialize()
-		}).done(function() {
-			$('.contacts__button').css('display', 'none');
-			$('.contacts__success').css('display', 'block');
-			setTimeout(function() {
-				th.trigger("reset");
-			}, 1000);
+	function validValue(el, phone) {
+		el.click(function(e) {
+			let regEx = new RegExp(/\+\d\(\d\d\d\)\d\d\d\-\d\d\-\d\d/);
+			if(!(regEx.test(phone.val()))) {
+				phone.css('border-bottom', '4px solid #ff7730');
+				e.preventDefault();
+			} else {
+				phone.css('border-bottom', 'none');
+			}
 		});
-		return false;
-	});
+	}
 
-	$(".modal__form").submit(function() { 
-		let th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: th.serialize()
-		}).done(function() {
-			$('.modal__button').css('display', 'none');
-			$('.modal__success').css('display', 'block');
-			setTimeout(function() {
-				th.trigger("reset");
-			}, 1000);
+	validValue($('.contacts__button'), $('.contacts__input_phone'));
+	validValue($('.modal__button'), $('.modal__input_phone'));
+
+
+
+
+	function validForm(el, button, success) {
+		el.submit(function() { 
+			let th = $(this);
+			$.ajax({
+				type: "POST",
+				url: "mail.php",
+				data: th.serialize()
+			}).done(function() {
+				button.css('display', 'none');
+				success.css('display', 'block');
+				setTimeout(function() {
+					th.trigger("reset");
+				}, 1000);
+			});
+			return false;
 		});
-		return false;
-	});
-	}();
+	}
+
+	validForm($(".contacts__form"), $('.contacts__button'), $('.contacts__success'));
+	validForm($(".modal__form"), $('.modal__button'), $('.modal__success'));
+}();
     
-    module.exports = sendform;
+module.exports = sendform;
